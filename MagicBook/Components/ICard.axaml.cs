2d864373fb879f604604696a3ba0f75a5c3b9e05
niyyzf-lab <niyyzf@hotmail.com>
2023-12-26 13:42:00 +0800

    完成卡片

--- MagicBook/Components/ICard.axaml.cs
+++ MagicBook/Components/ICard.axaml.cs
@@ -4,7 +4,51 @@ using Avalonia.Controls.Primitives;
 
 namespace MagicBook.Components;
 
-public class ICard : TemplatedControl
+public class ICard : Button
 {
-}
+    //Image
+    public static readonly StyledProperty<object?> ImageProperty = AvaloniaProperty.Register<ICard, object?>(
+        nameof(Image),null);
 
+    public object? Image
+    {
+        get => GetValue(ImageProperty);
+        set => SetValue(ImageProperty, value);
+    }
+    //Avatar
+    public static readonly StyledProperty<object?> AvatarProperty = AvaloniaProperty.Register<ICard, object?>(
+        nameof(Avatar),null);
+
+    public object? Avatar
+    {
+        get => GetValue(AvatarProperty);
+        set => SetValue(AvatarProperty, value);
+    }
+    //Title
+    public static readonly StyledProperty<object> TitleProperty = AvaloniaProperty.Register<ICard, object>(
+        nameof(Title),"Name");
+
+    public object Title
+    {
+        get => GetValue(TitleProperty);
+        set => SetValue(TitleProperty, value);
+    }
+    //LikeNumber
+    public static readonly StyledProperty<string?> LikeNumberProperty = AvaloniaProperty.Register<ICard, string?>(
+        "LikeNumber","0");
+
+    public string? LikeNumber
+    {
+        get => GetValue(LikeNumberProperty);
+        set => SetValue(LikeNumberProperty, value);
+    }
+    //BrowseNumber
+    public static readonly StyledProperty<string?> BrowseNumberProperty = AvaloniaProperty.Register<ICard, string?>(
+        nameof(BrowseNumber),"0");
+
+    public string? BrowseNumber
+    {
+        get => GetValue(BrowseNumberProperty);
+        set => SetValue(BrowseNumberProperty, value);
+    }
+}
